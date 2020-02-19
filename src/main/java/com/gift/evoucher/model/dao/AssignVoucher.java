5beee7f24662a8a218d37cad65919542454a6855
karan <>
2020-02-19 17:40:28 +0530

    assignvoucher dao

---
+++ src/main/java/com/gift/evoucher/model/dao/AssignVoucher.java
@@ -0,0 +1,32 @@
+package com.gift.evoucher.model.dao;
+
+import java.io.Serializable;
+
+public class AssignVoucher implements Serializable {
+    private Long mobileNo;
+    private String voucherId;
+
+    public AssignVoucher() {
+    }
+
+    public AssignVoucher(Long mobileNo, String voucherId) {
+        this.mobileNo = mobileNo;
+        this.voucherId = voucherId;
+    }
+
+    public Long getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(Long mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getVoucherId() {
+        return voucherId;
+    }
+
+    public void setVoucherId(String voucherId) {
+        this.voucherId = voucherId;
+    }
+}
